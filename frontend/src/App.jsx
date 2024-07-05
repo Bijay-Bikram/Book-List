6bcfd88eec60832018c7594495e58c30cef1178b
bijay <bijaybikram2244@gmail.com>
2024-07-06 04:53:43 +0545

    Removed  useless code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/Home'
 import CreateBook from './pages/CreateBook'
 import ShowBook from './pages/ShowBook'
 import EditBook from './pages/EditBook'
-import DeleteBook from './pages/DeleteBook'
 
 const App = () => {
   return (
